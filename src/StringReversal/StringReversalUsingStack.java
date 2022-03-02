9a7f580a5fca3c81dc6a0ac69d81348cfcb9a7fd
abhinavbhardwaj <abhinavbhardwaj68@gmail.com>
2022-03-03 00:54:41 +0530

    Entire logic required for reversing string using Stack.

--- src/StringReversal/StringReversalUsingStack.java
+++ src/StringReversal/StringReversalUsingStack.java
@@ -1,5 +1,7 @@
 package StringReversal;
 
+import java.util.*;
+
 /**
  * Created by IntelliJ IDEA
  * User: Abhinav Bhardwaj
@@ -10,4 +12,33 @@ package StringReversal;
 
 public class StringReversalUsingStack {
 
+    static String StringReversal (String s) {
+        char[] reversedStringArray = new char[s.length()];
+        Stack<Character> stack = new Stack<Character>();
+
+        int index;
+
+        for (index = 0; index < s.length(); index++) {
+            stack.push(s.charAt(index));
+        }
+
+        index = 0;
+
+        while (!stack.isEmpty()) {
+            reversedStringArray[index++] = stack.pop();
+        }
+
+        String reversedString = new String(reversedStringArray);
+
+        return reversedString;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.next();
+
+        String s = StringReversal(S);
+        System.out.println(s);
+    }
 }
